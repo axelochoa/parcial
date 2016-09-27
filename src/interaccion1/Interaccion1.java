820c8019021154ae53ff271cca1130ff59626168
AXEL-PC <AXEL-PC@192.168.29.12>
2016-09-27 17:05:23 -0300

    Iniciando Los Trabajos!

---
+++ src/interaccion1/Interaccion1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interaccion1;
+
+/**
+ *
+ * @author AXEL-PC
+ */
+public class Interaccion1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i=100 ; 1>=0; i--) {
+           
+        
+            System.out.println("hola");
+            System.out.println(i);
+            
+            
+            
+            
+        }
+    }
+    
+}
